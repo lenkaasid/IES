8d1f5eb69a2174b296c022247f450664ece8d8e4
lenkaasid <lenkaasid@gmail.com>
2023-10-16 13:23:15 +0530

    Initial commit

---
+++ GFG1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class GFG1 {
+	public static void main(String[] args)
+	{
+
+		// The cursor will remain
+		// just after the 1
+		System.out.print("GfG1");
+
+		// This will be printed
+		// just after the GfG2
+		System.out.print("GfG2");
+	}
+}
